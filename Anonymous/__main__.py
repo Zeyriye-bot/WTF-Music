923cb5f0fc0a83123c58cf55ff3b90de52154c1d
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2022-01-08 02:21:58 +0530

    Update __main__.py

--- Anonymous/__main__.py
+++ Anonymous/__main__.py
@@ -119,11 +119,7 @@ async def initiate_bot():
     console.print(f"\n[red]sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ")
 
 
-home_text_pm = f"""ʜᴇʏ,
-ᴍʏ ɴᴀᴍᴇ ɪs {BOT_NAME}.
-ɪ'ᴍ ᴛᴇʟᴇɢʀᴀᴍ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴀᴜᴅɪᴏ ᴡɪᴛʜ sᴏᴍᴇ ᴜsᴇꜰᴜʟ ꜰᴇᴀᴛᴜʀᴇs.
-
-ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ​: / """
+home_text_pm = await message.reply_sticker("CAADBAADWxcAAqbxcR4yeTJRtPe4UAI")
 
 
 @app.on_message(filters.command("help") & filters.private)
