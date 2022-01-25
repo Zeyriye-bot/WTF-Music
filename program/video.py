3c0b6330fbb6250c636be8b90f48212aa4385cd2
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-26 02:15:21 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -178,7 +178,7 @@ async def vplay(c: Client, m: Message):
                     "» reply to an **video file** or **give something to search.**"
                 )
             else:
-                loser = await c.send_message(chat_id, **Searching...**")
+                loser = await c.send_message(chat_id,"🔎 **Searching...**")
                 query = m.text.split(None, 1)[1]
                 search = ytsearch(query)
                 Q = 720
